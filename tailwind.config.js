8a67f5e8a0eb59fa04514d6d2527baaaf174bbfb
primado <primadoprincepa@gmail.com>
2023-01-11 10:26:08 +0000

    add templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}",],
+  content: ["./**/*.{html,js}",
+  "../templates/**/*.{html,js}",],
   theme: {
 
     colors: {
